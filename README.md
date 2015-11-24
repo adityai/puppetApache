20905570a2787c4c9586c72baefaacf4e3b9a160
Aditya Inapurapu <aialerts@yahoo.com>
2015-11-24 04:30:40 +0000

    First commit of puppet apache module

---
+++ README.md
@@ -0,0 +1,7 @@
+# puppetApache
+Simple puppet module to install Apache with Puppet.
+
+To run the puppet module execute the following command from
+puppetApache folder.
+
+sudo puppet apply --modulepath . runner.pp
---
+++ apache/manifests/init.pp
@@ -0,0 +1,32 @@
+#Module to provision a apache web server in centos, rhel and ubuntu
+class apache {
+  case $::operatingsystem {
+    "ubuntu": {
+      $packagename = "apache2"
+      $servicename = "apache2"
+    }
+    "centos", "rhel": {
+      $packagename = "httpd"
+      $sercicename = "httpd"
+    }
+    default: {
+      fail("Ubsupported OS: ${::operatingsystem}")
+    }
+  }
+
+  #Ensure Apache is installed
+  package { 'apache':
+    name    =>    $packagename,
+    ensure  =>    installed,
+  }
+
+  #Ensure Apache is running
+  service { 'apache':
+    name      =>  $servicename,
+    ensure    =>  running,
+    require   =>  Package['apache'],
+  }
+
+
+}
+
